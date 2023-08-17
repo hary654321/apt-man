1048eb603cfa5cfe948bce63a1b4e6cb04624c8b
wangrui <hary654321@126.com>
2023-08-17 16:47:53 +0800

    LONGTEXT

--- sql/update.sql
+++ sql/update.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `probe_result` ADD COLUMN `remark` VARCHAR(255) NOT NULL DEFAULT '' AFTER `response`;
 ALTER TABLE `probe_result` ADD COLUMN `dealed` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1 未处理  2已处理' AFTER `response`;
 ALTER TABLE `port_result` ADD COLUMN `remark` VARCHAR(255) NOT NULL DEFAULT '' AFTER `response`;
+ALTER TABLE `plug_result` CHANGE COLUMN `res` `res` LONGTEXT NOT NULL COLLATE 'utf8_general_ci' AFTER `type`;
