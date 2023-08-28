0900bfb6f94bc7f2451c6d990f33361862b41942
wangrui <hary654321@126.com>
2023-08-28 10:12:50 +0800

    res

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 git pull
-go build -o apt apt.go
-ps -ef | grep ./apt | grep -v grep | awk '{print $2}' | xargs kill -9
-cd /u2/apt/www/strategy-manage && ./apt 138.toml>> m.log &
+go build -o apt-man main.go
+ps -ef | grep ./apt-man | grep -v grep | awk '{print $2}' | xargs kill -9
+cd /u2/apt-man && ./apt-man 138.toml>> m.log &
