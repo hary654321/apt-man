d39e699a718644fd76ae8b4d4139e34d4b0e4a2b
wangrui <hary654321@126.com>
2023-09-27 15:42:12 +0800

    gj

---
+++ gj.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+array=("cyberspace-resources_zg" "cyberspace-resources_yg" "cyberspace-resources_hg","cyberspace-resources_gd1","cyberspace-resources_fg","cyberspace-resources_ads2")
+
+for element in "${array[@]}"
+do
+    echo $element
+    ps -ef | grep cyqy | grep -v grep | awk '{print $2}' | xargs kill -9
+    cd /u4/logstash-7.9.3
+
+    yes|cp -f config/cyqy.base config/cyqy.conf
+    sed -i "s/day/$element/g" config/cyqy.conf
+    rm -rf data
+    ./bin/logstash -f config/cyqy.conf
+
+done
