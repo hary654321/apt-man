ceebfa7642e495f8258ad17d69983284513170e7
wangrui <hary654321@126.com>
2023-10-09 14:36:55 +0800

    走物理机

--- update/wl.sh
+++ update/wl.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-docker stop crocodile_server
-docker stop crocodile_scaner
-mkdir -p /zrtx/apt/scan
-mkdir -p /app
-cp -rf  ./client/* /zrtx/apt/scan
+mkdir -p /zrtx/apt
+cp -rf ./client/* /zrtx/apt
 ulimit -n 50000
 monitorM=`ps -ef | grep apt-server | grep -v grep | wc -l ` 
 if [ $monitorM -eq 0 ] 
 then
 	echo "apt-server is not running, restart apt-server "
-	 ./apt-server  wl.toml>>m.log &
+	nohup ./apt-server  conf.toml>>m.log &
 else
 	echo "apt-server is running"
 fi
@@ -19,8 +16,12 @@ monitorS=`ps -ef | grep apt-scan| grep -v grep | wc -l `
 if [ $monitorS -eq 0 ] 
 then
 	echo "apt-scan not running, restart apt-scan"
-        cd /zrtx/apt/scan  && ./apt-scan >>s.log &
+	nohup   /zrtx/apt/apt-scan >>s.log &
 else
 	echo "apt-scan is running"
 fi
+
+
+
+
 
