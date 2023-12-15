48ed9f4bc2b322c8ed07fb36e0f5cce2c914c436
wangrui <hary654321@126.com>
2023-12-15 11:24:51 +0800

    部署测试成功

--- x86/client/clientInstall.sh
+++ x86/client/clientInstall.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-mkdir -p  /zrtx/config/cyberspace
-mkdir -p  /zrtx/log/cyberspace
 cd /zrtx/apt
 chmod +x apt-scan
-#go build -o worker worker.go
 ps -ef | grep ./apt-scan | grep -v grep | awk '{print $2}' | xargs kill -9
-./apt-scan  >> /zrtx/log/cyberspace/apt-scan.log &
+/zrtx/apt/apt-scan  >> /zrtx/apt/s.log &
