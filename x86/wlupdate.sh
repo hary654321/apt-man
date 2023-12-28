5589873f6371f56dbb25b7c5e5a47bd51b8b761c
wangrui <hary654321@126.com>
2023-12-28 11:01:52 +0800

    页面更新

--- x86/wlupdate.sh
+++ x86/wlupdate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-docker stop crocodile_server
-docker stop crocodile_scaner
+mkdir -p /zrtx/apt
+mkdir -p /app
 
 ps -ef | grep apt-scan| grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep apt-server| grep -v grep | awk '{print $2}' | xargs kill -9
-mkdir -p /zrtx/apt
-mkdir -p /app
+
 yes | cp -rf  ./client/* /zrtx/apt
 ulimit -n 50000
 
+nohup ./apt-server  wl.toml>>m.log &
 nohup ./apt-server  wl.toml>>m.log &
 cd /zrtx/apt  && nohup  ./apt-scan >>s.log &
 
