c32410800a27d25fe163402ddb022aca45a357a6
wangrui <hary654321@126.com>
2023-12-28 10:01:09 +0800

    兼容模式

--- x86/client/clientInstall.sh
+++ x86/client/clientInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /zrtx/apt
-chmod +x apt-scan
+chmod +x apt-scan-`uname -m`
 ps -ef | grep ./apt-scan | grep -v grep | awk '{print $2}' | xargs kill -9
-/zrtx/apt/apt-scan  >> /zrtx/apt/s.log &
+/zrtx/apt/apt-scan-`uname -m`  >> /zrtx/apt/s.log &
